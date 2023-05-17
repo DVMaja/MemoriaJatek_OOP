fe16bceb885dd5d9017b2076d9b2ad7a347d7e18
DVMaja <majadreilinger@gmail.com>
2023-05-17 10:58:16 +0200

    Kartya példányosítva

--- JS/Classes/Jatekter.js
+++ JS/Classes/Jatekter.js
@@ -1,24 +1,22 @@
+import Kartya from "./Kartya.js";
+
 class Jatekter {
     #kartyaLista = [];
     #kivalasztottKartyaLista = [];
 
-    constructor(kartyaLista){
-        this.#kartyaLista = kartyaLista;
-        this.#kivalasztottKartyaLista;
-
+    constructor(kartyaLista) {
+        this.#kartyaLista = kartyaLista;        
         const szuloELEM = $(".szuloELEM");
 
         for (let index = 0; index < this.#kartyaLista.length; index++) {
-            //Kartya példányosítása
-            
+            new Kartya(this.#kartyaLista[index].kep, false, szuloELEM);
         }
-
     }
 
-    #init(){}
-    #kever(){}
-    #ellenorzes(){}
-    #TriggerBlocked(){}
-    #TriggerUnBlocked(){}
+    #init() { }
+    #kever() { }
+    #ellenorzes() { }
+    #TriggerBlocked() { }
+    #TriggerUnBlocked() { }
 }
 export default Jatekter;
