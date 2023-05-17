e651f2086e33c089b94226df16803110b75ca208
DVMaja <majadreilinger@gmail.com>
2023-05-17 10:44:01 +0200

    Kartya.js létrehozva

---
+++ JS/Classes/Kartya.js
@@ -0,0 +1,33 @@
+class Kartya {
+    #fajlNev;   //String
+    #allapot;   //boolean
+    #blokkolt   //boolean
+    #divELEM;
+    #imgELEM;
+
+
+    constructor(id, allapot, szuloELEM){
+        this.#fajlNev;
+        this.#allapot = allapot;
+        this.#blokkolt;        
+        this.#divELEM = szuloELEM;
+        this.#imgELEM;
+    }
+
+    #setAllapot(){
+
+    }
+    getFajlnev(){
+
+    }
+
+    #setLap(){
+
+    }
+
+    #kattintasTrigger(){
+        //forditnevű sajat esemeny
+    }
+}
+
+export default Kartya;
