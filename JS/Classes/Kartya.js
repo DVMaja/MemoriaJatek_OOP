a583afc1ec3aa46440186eaa746b9230362c2d90
DVMaja <majadreilinger@gmail.com>
2023-05-17 11:16:24 +0200

    kattintasTrigger  kész

--- JS/Classes/Kartya.js
+++ JS/Classes/Kartya.js
@@ -1,32 +1,37 @@
 class Kartya {
-    #fajlNev;   //String
-    #allapot;   //boolean
-    #blokkolt   //boolean
+    #fajlNev; //String
+    #allapot; //boolean
+    #blokkolt; //boolean
     #divELEM;
     #imgELEM;
 
-
-    constructor(id, allapot, szuloELEM){
-        this.#fajlNev;
+    constructor(fajlNev, allapot, szuloELEM) {
+        this.#fajlNev = fajlNev;
         this.#allapot = allapot;
-        this.#blokkolt;        
+        this.#blokkolt;
+        szuloELEM.append(`<div class="kartya"> <img src="Stilus/kepek/${this.#fajlNev}" alt=""></div>`);
+
         this.#divELEM = szuloELEM;
         this.#imgELEM;
-    }
 
-    #setAllapot(){
-
-    }
-    getFajlnev(){
 
     }
 
-    #setLap(){
+    #setAllapot() { 
+        
+    }
+    getFajlnev() { 
 
     }
 
-    #kattintasTrigger(){
+    #setLap() { 
+
+    }
+
+    #kattintasTrigger() {
         //forditnevű sajat esemeny
+        const esemeny = new CustomEvent("fordit", { detail: this });
+        window.dispatchEvent(esemeny);
     }
 }
 
