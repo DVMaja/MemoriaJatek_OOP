06b7c0f76f8048bc424357a2677a4831e511fd23
DVMaja <majadreilinger@gmail.com>
2023-05-17 10:46:13 +0200

    Jatekter.js létrehozva

---
+++ JS/Classes/Jatekter.js
@@ -0,0 +1,24 @@
+class Jatekter {
+    #kartyaLista = [];
+    #kivalasztottKartyaLista = [];
+
+    constructor(kartyaLista){
+        this.#kartyaLista = kartyaLista;
+        this.#kivalasztottKartyaLista;
+
+        const szuloELEM = $(".szuloELEM");
+
+        for (let index = 0; index < this.#kartyaLista.length; index++) {
+            //Kartya példányosítása
+            
+        }
+
+    }
+
+    #init(){}
+    #kever(){}
+    #ellenorzes(){}
+    #TriggerBlocked(){}
+    #TriggerUnBlocked(){}
+}
+export default Jatekter;
